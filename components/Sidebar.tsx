b8fc628fd840192278a2bc0b83cb91692afd8c02
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-06 18:16:05 +0900

    Update sidebar links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = ({user}: SiderbarProps) => {
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
             <Link href="/" 
-            className="mb-12 cursor-pointer items-center gap-2">
+            className="flex mb-12 cursor-pointer items-center gap-2">
                 <Image src="/icons/logo.svg" width={34} height={34} alt="BiBiBank" className="size-[24px] max-xl:size-14" />
                 <h1 className="sidebar-logo">BiBiBank</h1>
             </Link>
@@ -20,10 +20,17 @@ const Sidebar = ({user}: SiderbarProps) => {
             {sidebarLinks.map((item) => {
                 const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
                 return (
-                    <Link href={item.route} key={item.label} className={cn("sidebar-link", {'bg-bank-gradient': isActive})}>{item.label}</Link>
+                    <Link href={item.route} key={item.label} className={cn("sidebar-link", {'bg-bank-gradient': isActive})}>
+                        <div className="relative size-6">
+                            <Image src={item.imgURL} alt={item.label} fill className={cn({"brightness-[3] invert-0": isActive})}/>
+                        </div>
+                        <p className={cn("sidebar-label", {'!text-white':isActive})}>{item.label}</p>
+                    </Link>
                 )
-})}
+})}     
+        USER
         </nav>
+        USEROUT
     </section>
   )
 }
