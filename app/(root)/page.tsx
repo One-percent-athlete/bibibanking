3964e20792dd984ca11b45439da2ffb35316e810
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-06 16:53:21 +0900

    Create total balance box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox"
+import TotalBanlancebox from "@/components/TotalBanlancebox"
 import React from 'react'
 
 const Home = () => {
@@ -8,9 +9,14 @@ const Home = () => {
         <div className="home-content">
             <header className="home-header">
                 <HeaderBox type="greeting"
-                title="Welcome"
-                user={loggedIn?.firstName || "Guest"} 
-                subtext="Access and manage your account and transactions efficiently" />
+                    title="Welcome"
+                    user={loggedIn?.firstName || "Guest"} 
+                    subtext="Access and manage your account and transactions efficiently" />
+                <TotalBanlancebox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1234.56}
+                />
             </header>
         </div>    
     </section>
