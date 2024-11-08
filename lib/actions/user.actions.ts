35df0dcb17bf443f1565cfc3712ba56f4f192956
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-08 15:11:10 +0900

    Update appwrite and user action files to config with the database

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,8 @@
 'use server';
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+
 // import { ID, Query } from "node-appwrite";
 // import { createAdminClient, createSessionClient } from "../appwrite";
 // import { cookies } from "next/headers";
@@ -303,9 +306,33 @@ export const signIn = async () => {
 }
 
 export const signUp = async (userData: SignUpParams) => {
+
+  const {email, password, firstName, lastName} = userData
+
   try {
+    const { account } = await createAdminClient();
+
+  const newUserAccount =  await account.create(ID.unique(), email, password, `${firstName} ${lastName}`);
+  const session = await account.createEmailPasswordSession(email, password);
+
+  cookies().set("my-custom-session", session.secret, {
+    path: "/",
+    httpOnly: true,
+    sameSite: "strict",
+    secure: true,
+  });
 
   } catch (error) {
     console.log(error);
+  }
+}
+
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
   }
 }
