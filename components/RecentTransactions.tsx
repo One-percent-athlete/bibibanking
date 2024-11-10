0486e8fdec26567d54bb77679d990d7d1c9692e0
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-10 20:34:21 +0900

    import shacdn table

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { BankTabItem } from "./BankTabItem"
 import BankInfo from "./BankInfo"
+import TransactionsTable from "./TransactionsTable"
 
 const RecentTransactions = ({ accounts, transactions = [], appwriteItemId, page = 1 }: RecentTransactionsProps) => {
   return (
@@ -21,6 +22,7 @@ const RecentTransactions = ({ accounts, transactions = [], appwriteItemId, page 
             {accounts.map((account:Account) => (
                 <TabsContent value={account.appwriteItemId} key={account.id} className="space-y-4">
                     <BankInfo account={account} appwriteItemId={appwriteItemId} type="full" />
+                    <TransactionsTable transactions={transactions} />
                 </TabsContent>
             ))}
         </Tabs>
