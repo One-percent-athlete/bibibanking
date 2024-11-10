96ea9cbda061ca4d93e1525cda77cc30ff16c028
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-10 20:30:23 +0900

    Update recent transaction component

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { BankTabItem } from "./BankTabItem"
+import BankInfo from "./BankInfo"
 
 const RecentTransactions = ({ accounts, transactions = [], appwriteItemId, page = 1 }: RecentTransactionsProps) => {
   return (
@@ -19,7 +20,7 @@ const RecentTransactions = ({ accounts, transactions = [], appwriteItemId, page 
             </TabsList>
             {accounts.map((account:Account) => (
                 <TabsContent value={account.appwriteItemId} key={account.id} className="space-y-4">
-                    
+                    <BankInfo account={account} appwriteItemId={appwriteItemId} type="full" />
                 </TabsContent>
             ))}
         </Tabs>
