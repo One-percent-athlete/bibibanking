cf92096c4dbef37325dc7b7229441a47335fc791
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-11 20:54:43 +0900

    Implement transactions table to transation history page

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox"
+import TransactionsTable from "@/components/TransactionsTable";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import { formatAmount } from "@/lib/utils";
@@ -42,6 +43,9 @@ const TransactionHistory = async ({ searchParams: {id, page}}: SearchParamProps)
           </div>
         </div>
 
+        <section className="flex flex-col w-full gap-6">
+          <TransactionsTable transactions={account?.transactions}/>
+        </section>
       </div>
     </div>
   )
