3e2dc7c5fb63cc141a6bf1fb2e797b0da095d767
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-11 20:18:32 +0900

    Update sidebar and plaid link components

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,5 +1,5 @@
 import { createLinkToken, exchangePublicToken } from "@/lib/actions/user.actions"
-import { StyledString } from "next/dist/build/swc/types"
+import Image from "next/image"
 import { useRouter } from "next/navigation"
 import React, { useCallback, useEffect, useState } from 'react'
 import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from "react-plaid-link"
@@ -37,10 +37,20 @@ const PlaidLink = ({user, variant}: PlaidLinkProps ) => {
     <>
         {variant === "primary" ? (
             <button onClick={() => open()} disabled={!ready} className="plaidlink-primary py-2">Connect Bank</button>
-        ) : variant === "ghost" ?(
-            <button>Connect Bank</button>
+        ) : variant === "ghost" ? (
+            <button onClick={()=> open()} className="plaidlink-ghost">
+                <Image src="/icons/connect-bank.svg" alt="connect bank" height={24} width={24} />
+                <p className="hidden text-[16px] font-semibold text-black-2 xl:block">
+                Connect Bank
+                </p>
+            </button>
         ) : (
-            <button>Connect Bank</button>
+            <button onClick={()=> open()} className="plaidlink-default">
+                <Image src="/icons/connect-bank.svg" alt="connect bank" height={24} width={24} />
+                <p className="text-[16px] font-semibold text-black-2">
+                Connect Bank
+                </p>
+            </button>
         )}
     </>
   )
