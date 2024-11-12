8d84e5f3d4777bc9a4b0eb073f78d6e0f46031a0
One-percent-athlete <one.percent.athlete@gmail.com>
2024-11-12 13:29:22 +0900

    Update payment transfer page

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,7 +1,20 @@
 import HeaderBox from "@/components/HeaderBox"
+import PaymentTransferForm from "@/components/PaymentTransferForm"
+import { getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from 'react'
 
-const PaymentTransfer = () => {
+const PaymentTransfer = async () => {
+
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
+
+  if(!accounts) return;
+  
+  const accountsData = accounts?.data;
+
   return (
     <section className="payment-transfer">
       <HeaderBox 
@@ -9,8 +22,8 @@ const PaymentTransfer = () => {
         subtext="Please provide specific details or notes related to the payment transfer"
       />
       <section className="size-full pt-5">
-        
-      </section>
+        <PaymentTransferForm accounts={accountsData} />
+      </section>  
     </section>
   )
 }
